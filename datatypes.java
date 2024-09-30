c1c75569ed650f8defebaec3522a9a806f345c3a
Akash Chauhan <ac1675229@gmail.com>
2024-09-30 21:58:46 +0530

    second

---
+++ datatypes.java
@@ -0,0 +1,12 @@
+public class datatypes {
+    public static void main(String[] args) {
+        int a = 4;
+        long l = 8586l;
+        short sh = 756;
+        byte bt = 127;
+        System.out.println(a);
+        System.out.println(l);
+        System.out.println(sh);
+        System.out.println(bt);
+    }
+}
