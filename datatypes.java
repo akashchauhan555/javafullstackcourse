bc6258d7c7d6130955b3de9e10e41fb5a4b4f6d6
Akash Chauhan <ac1675229@gmail.com>
2024-10-03 21:35:50 +0530

    done

--- datatypes.java
+++ datatypes.java
@@ -4,9 +4,17 @@ public class datatypes {
         long l = 8586l;
         short sh = 756;
         byte bt = 127;
+        float f = 5.5f;
+        double d = 55.5d;
+        char c = '5';              // 8 is literal
+        boolean b = true;
         System.out.println(a);
         System.out.println(l);
         System.out.println(sh);
         System.out.println(bt);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(b);
     }
 }
